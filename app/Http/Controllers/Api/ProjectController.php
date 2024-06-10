b023aec3577f5655f34ac55228be5cb1fd5ca192
Nik0-9 <filannino.nicola9@gmail.com>
2024-06-10 17:00:39 +0200

    ADDED paginate

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(5);
         //dd($projects);
         return response()->json([
             'success' => true,
