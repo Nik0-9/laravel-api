e37d1cf6566c957f108912422740a0e3f9438e93
Nik0-9 <filannino.nicola9@gmail.com>
2024-06-10 15:43:14 +0200

    CREATE ProjectControlle with namespace App\Http\Controllers\Api ADDED function index. ADDED Route get projects index

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use GrahamCampbell\ResultType\Success;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        //dd($projects);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\ProjectController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('projects', [ProjectController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
