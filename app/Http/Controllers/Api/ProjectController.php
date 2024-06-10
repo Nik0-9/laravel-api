a013111ee1435d078b8ca28fef1d2eafaf7335a4
Nik0-9 <filannino.nicola9@gmail.com>
2024-06-10 16:57:54 +0200

    ADDED function show of Api\ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -18,5 +18,19 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
-    
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Project not found'
+            ]);
+        }
+    }
 }
