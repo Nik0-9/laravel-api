c8328aab6603df95ac88317f062a9cdeed0d1588
Nik0-9 <filannino.nicola9@gmail.com>
2024-06-13 11:58:51 +0200

    CREATED layout mail recived

---
+++ resources/views/mails/new-contact.blade.php
@@ -0,0 +1,7 @@
+<h1>Ciao Admin!</h1>
+<p>
+    Hai ricevuto un nuovo messaggio dal tuo sito <br>
+    <strong>Name:</strong> <span>{{ $lead->name }}</span> <br>
+    <strong>Email:</strong> <span>{{ $lead->address }}</span> <br>
+    <strong>Messaggio:</strong> <span>{{ $lead->message }}</span> <br>
+</p>
